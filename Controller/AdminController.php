0ecfcae98b72b8d2701007f3cd61a21e88f08a6e
Julien Fastré <julien.fastre@champs-libres.coop>
2015-09-29 17:18:29 +0200

    fix layout and menu for activity configuration
    close #614 1h00

---
+++ Controller/AdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Chill is a software for social workers
+ * Copyright (C) 2015 Champs Libres <info@champs-libres.coop>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Chill\ActivityBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Controller for activity configuration
+ *
+ * @author Julien Fastré <julien.fastre@champs-libres.coop>
+ * @author Champs Libres <info@champs-libres.coop>
+ */
+class AdminController extends Controller
+{
+    public function indexActivityAction()
+    {
+        return $this->render('ChillActivityBundle:Admin:layout_activity.html.twig');
+    }
+}
